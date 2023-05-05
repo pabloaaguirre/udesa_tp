f0916e92ed21462ea07ab63c74ce42c015aa4f5e
Pablo Aguirre <pabloaaguirre@google.com>
2023-05-05 11:48:48 -0300

    conn test

--- scripts/recomendations_model/connection_test.py
+++ scripts/recomendations_model/connection_test.py
@@ -72,7 +72,7 @@ def rds_conn_test():
     )
 
     cursor.execute(
-        """"
+        """
         INSERT INTO recomendations(advertiser_id, product_id, model)
         VALUES ('advertiser123', 'product123', 'top_product');
         """
